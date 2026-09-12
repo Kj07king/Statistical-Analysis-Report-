4b6c30b1932b18898d34aebf6d4a239f6e6b9b33
Kj07king <kabirkimtani108@gmail.com>
2026-09-12 19:08:45 +0200

    Clean and process know_exploited_vulnerabilities data

--- Final_analysis_code_file.R
+++ Final_analysis_code_file.R
@@ -1,1 +1,12 @@
 
+#cleaning know_exploited_vulnerabilities
+cleaned_KEV_df <- read.csv("know_exploited_vulberabilities.csv" , stringsAsFactors = False) %>%
+  mutate(
+    dateAdded = as.Data(dateAdded),
+    dueDate = as.Data(dueDate),
+    Remediation_Days = as.numeric(difftime(dueDate, dateAdded, units = "days"))
+  ) %>%
+filter(Remediation_day >= 0) %>%
+select(cveID, dateAdded, dueDate, Remediation_Days)
+
+
